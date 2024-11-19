20b58d9f0e153c0f09a5f125d7616fac91fdbd45
kkanden <mroskarez@gmail.com>
2024-11-19 21:20:09 +0100

    format file

--- global.R
+++ global.R
@@ -21,27 +21,27 @@ library(shinytitle)
 
 
 connect <- function() {
-  con <- DBI::dbConnect(RPostgres::Postgres(),
-    host = "kanden-kanden-b7d6.d.aivencloud.com",
-    dbname = "defaultdb",
-    user = config::get("user"),
-    password = config::get("pwd"),
-    port = 13499
-  )
+    con <- DBI::dbConnect(RPostgres::Postgres(),
+        host = "kanden-kanden-b7d6.d.aivencloud.com",
+        dbname = "defaultdb",
+        user = config::get("user"),
+        password = config::get("pwd"),
+        port = 13499
+    )
 }
 
 disconnect <- function(con) {
-  DBI::dbDisconnect(con)
+    DBI::dbDisconnect(con)
 }
 
 con <- connect()
 
 
 completion_times <- dbGetQuery(con, "SELECT * FROM completion_times") |>
-  as.data.table()
+    as.data.table()
 
 
 players <- dbGetQuery(con, "SELECT * FROM players") |>
-  as.data.table()
+    as.data.table()
 
 disconnect(con)
